d3eefc9334cb683fb33e706a6361a166fb15e5cf
Omran Suleiman <118446996+OmranSuleiman@users.noreply.github.com>
2025-01-29 14:46:39 +0100

    sista ändringar layout.

--- app/src/main/java/com/example/weatherapplicationproject/MainActivity.java
+++ app/src/main/java/com/example/weatherapplicationproject/MainActivity.java
@@ -2,8 +2,13 @@ package com.example.weatherapplicationproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
