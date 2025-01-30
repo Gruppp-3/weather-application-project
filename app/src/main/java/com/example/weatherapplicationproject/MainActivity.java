76a12100864c412fe12d15e4d40cc1b548c5defb
emiliaberglund <embe2203@student.miun.se>
2025-01-30 11:01:16 +0100

    wind

--- app/src/main/java/com/example/weatherapplicationproject/MainActivity.java
+++ app/src/main/java/com/example/weatherapplicationproject/MainActivity.java
@@ -3,24 +3,38 @@ package com.example.weatherapplicationproject;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.weatherapplicationproject.model.WeatherApi;
+import com.example.weatherapplicationproject.model.WeatherForecast;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView  tempText, humText, descText, windText;
+    private TextView cityNameText, tempText, humText, descText, windText;
     private ImageView weatherIcon;
     private Button refreshButton;
+    private EditText cityNameInput;
 
+    WeatherApi api = new WeatherApi();
+    WeatherForecast forecast = api.callWeatherAPI();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        descText = findViewById(R.id.descText);
 
+        descText = findViewById(R.id.descText);
+        windText = findViewById(R.id.windText);
+
+        api = new WeatherApi();
 
         updateCloudiness("Sunny %");
-
+        updateWind(forecast.getWindSpeed());
 
     }
 
@@ -28,4 +42,18 @@ public class MainActivity extends AppCompatActivity {
         if (descText != null) descText.setText(description);
 
     }
+
+    private void updateWind(double windSpeed) {
+        if (windText != null) {
+            windText.setText(String.format("%.0f km/h", windSpeed));
+        }
+    }
 }
+
+
+
+
+
+
+
+
