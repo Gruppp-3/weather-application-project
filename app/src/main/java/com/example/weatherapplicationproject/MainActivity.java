13c678082e93c1317873d3d5dd5c5f29382951e8
dane743 <dane2200@student.miun.se>
2025-01-30 10:09:54 +0100

    I implement descText

--- app/src/main/java/com/example/weatherapplicationproject/MainActivity.java
+++ app/src/main/java/com/example/weatherapplicationproject/MainActivity.java
@@ -2,19 +2,30 @@ package com.example.weatherapplicationproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView  tempText, humText, descText, windText;
     private ImageView weatherIcon;
     private Button refreshButton;
-    private EditText cityNameInput;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        descText = findViewById(R.id.descText);
+
+
+        updateCloudiness("Sunny %");
+
+
+    }
+
+    private void updateCloudiness(String description){
+        if (descText != null) descText.setText(description);
 
     }
 }
