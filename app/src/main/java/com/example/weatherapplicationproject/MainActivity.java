18143d6dd24b4f979327c93ff4893ae001072248
emiliaberglund <embe2203@student.miun.se>
2025-01-30 11:31:10 +0100

    wind

--- app/src/main/java/com/example/weatherapplicationproject/MainActivity.java
+++ app/src/main/java/com/example/weatherapplicationproject/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+
         descText = findViewById(R.id.descText);
         windText = findViewById(R.id.windText);
 
